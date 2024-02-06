ac03b96982677864f1838579f9d4504f9670877a
Gearhead Gladiators FTC <157541431+gearheadgladiatorsftc@users.noreply.github.com>
2024-02-06 16:19:22 -0500

    Formatted document

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,7 +13,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "Competition", group = "Competition")
-public class Main extends LinearOpMode {//#########################################################################################################
+public class Main extends LinearOpMode {// #########################################################################################################
 
   private DcMotor frontRightMotor;
   private DcMotor backRightMotor;
@@ -35,13 +36,13 @@ public class Main extends LinearOpMode {//######################################
     double denominator;
 
     boolean pixelLoaded = false;
-    boolean grabbed = false; //is the pixel grabbed?
+    boolean grabbed = false; // is the pixel grabbed?
 
     // threshold of low battery variable
     float lowBatThreshold = 9.0;
 
-    boolean autobreak = true; //check if slide autobreak is on
-    boolean slideDown = true; //Check if the slide is down
+    boolean autobreak = true; // check if slide autobreak is on
+    boolean slideDown = true; // Check if the slide is down
 
     frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
     backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
@@ -68,25 +69,28 @@ public class Main extends LinearOpMode {//######################################
       telemetry.update();
     }
     // Put initialization blocks here.
-    //telemetry.addData(">>",
-    //huskylensAsHuskyLens.knock() ? "Touch start to continue" : "Problem communicating with HuskyLens");
+    // telemetry.addData(">>",
+    // huskylensAsHuskyLens.knock() ? "Touch start to continue" : "Problem
+    // communicating with HuskyLens");
     // huskylensAsHuskyLens.selectAlgorithm();
-    //telemetry.update();
+    // telemetry.update();
     myElapsedTime = new ElapsedTime();
 
     sOut.setState(true);
 
     // Retrieve the IMU from the hardware map
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
+    IMU imu = hardwareMap.get(IMU.class, "imu");
+    // Adjust the orientation parameters to match your robot
+    IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+    // Without this, the REV Hub's orientation is assumed to be logo up / USB
+    // forward
+    imu.initialize(parameters);
 
     waitForStart();
-    if (isStopRequested()) return;//IDK what this does, but it was in the sample code, so we'll leave it.
+    if (isStopRequested())
+      return;// IDK what this does, but it was in the sample code, so we'll leave it.
     while (opModeIsActive()) {
       updateGPIO();
       mechanumLoop(); // Run the loop for the mechanum drive
@@ -96,7 +100,7 @@ public class Main extends LinearOpMode {//######################################
 
   }
 
-   static void huskyLoop() {/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  static void huskyLoop() {/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /*
      * if (myElapsedTime.seconds() >= 1) {
      * myElapsedTime.reset();
@@ -112,43 +116,46 @@ public class Main extends LinearOpMode {//######################################
      * }
      */
   }
-  static void mechanumLoop(){
-    //code to switch functions
+
+  static void mechanumLoop() {
+    // code to switch functions
   }
+
   public void fieldCentricLoop() {/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-   double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+    double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+    double x = gamepad1.left_stick_x;
+    double rx = gamepad1.right_stick_x;
 
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
+    // This button choice was made so that it is hard to hit on accident,
+    // it can be freely changed based on preference.
+    // The equivalent button is start on Xbox-style controllers.
+    if (gamepad1.options) {
+      imu.resetYaw();
+    }
 
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+    // Rotate the movement direction counter to the bot's rotation
+    double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+    double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
+    rotX = rotX * 1.1; // Counteract imperfect strafing
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+    // Denominator is the largest motor power (absolute value) or 1
+    // This ensures all the powers maintain the same ratio,
+    // but only if at least one is out of the range [-1, 1]
+    double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+    double frontLeftPower = (rotY + rotX + rx) / denominator;
+    double backLeftPower = (rotY - rotX + rx) / denominator;
+    double frontRightPower = (rotY - rotX - rx) / denominator;
+    double backRightPower = (rotY + rotX - rx) / denominator;
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+    frontLeftMotor.setPower(frontLeftPower);
+    backLeftMotor.setPower(backLeftPower);
+    frontRightMotor.setPower(frontRightPower);
+    backRightMotor.setPower(backRightPower);
   }
+
   public void robotCentricLoop() {/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Remember, Y stick value is reversed
     y = -gamepad1.left_stick_y;
@@ -166,10 +173,12 @@ public class Main extends LinearOpMode {//######################################
     frontRightMotor.setPower(((y - x) - rx) / denominator);
     backRightMotor.setPower(((y + x) - rx) / denominator);
   }
-  public boolean checkIfDown(){ //Method that checks to see if the slide is resting against the robot
-    //Code to use distance sensor
+
+  public boolean checkIfDown() { // Method that checks to see if the slide is resting against the robot
+    // Code to use distance sensor
     return true;
   }
+
   public void slideLoop(){/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(gamepad2.b && !autobreak){
       //brake motor
@@ -212,6 +221,7 @@ public class Main extends LinearOpMode {//######################################
   // Also, call resetAutobreakButton() when the op mode is starting, or when needed to reset the button state.
   
   }
+
   public void clawLoop(){//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if (gamepad2.y && pixelLoaded && !checkIfDown() && !liftMovingDown) {
       lift.setPower(-1);
@@ -248,7 +258,8 @@ public class Main extends LinearOpMode {//######################################
         while(gamepad2.a){}
     }
   }
-  public void updateGPIO(){
+
+  public void updateGPIO() {
     slideDown = ((GpioPinDigitalInput) sIn).getState();
   }
 }
